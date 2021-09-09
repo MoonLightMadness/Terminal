e58392926fb69025670a28806de7c97ba840b7fe
zhl <2377459837@qq.com>
2021-09-09 15:12:23 +0800

    完善配置

---
+++ src/app/host/Host.java
@@ -0,0 +1,19 @@
+package app.host;
+
+import app.config.Configer;
+
+import java.nio.channels.SocketChannel;
+
+/**
+ * @ClassName : app.host.Host
+ * @Description :
+ * @Date 2021-09-09 15:07:59
+ * @Author ZhangHL
+ */
+public class Host {
+
+    private SocketChannel socketChannel;
+
+    private Configer configer;
+
+}
