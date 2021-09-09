763be1e580a4931ef9ab38eeda3e855cb1a795ac
zhl <2377459837@qq.com>
2021-09-09 15:24:18 +0800

    精简包

---
+++ src/app/utils/GeneralUtil.java
@@ -0,0 +1,33 @@
+package app.utils;
+
+import app.config.Configer;
+import app.log.LogSystem;
+import app.log.LogSystemFactory;
+
+/**
+ * @ClassName : app.utils.GeneralUtil
+ * @Description :
+ * @Date 2021-09-09 15:18:00
+ * @Author ZhangHL
+ */
+
+public class GeneralUtil {
+
+    private static Configer configer = new Configer();
+
+    private static LogSystem log = LogSystemFactory.getLogSystem();
+
+    public static String readConfig(String property){
+        return configer.readConfig(property);
+    }
+
+    public static void info(String msg,Object... args){
+        log.info(msg, args);
+    }
+
+    public static void error(String msg,Object... args){
+        log.error(msg, args);
+    }
+
+
+}
